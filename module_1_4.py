016272bd656048c1fef18b4a7aa643a2a5a380bc
MaximVoronez <maxsemerikov2012@mail.ru>
2024-08-07 23:01:38 +0300

    Организация программ и методы строк

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+my_string = input('Введите пароль ввиде слова:')
+print('Пароль принят:', my_string)
+print('Хорошего дня:', my_string.upper())
+print('Хорошего дня:', my_string.lower())
+print('Хорошего дня:'.replace('Хорошего', 'Прекрасного'), 'Ваш пароль :', my_string)
+print('Прекрасного дня и удачи:'.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
